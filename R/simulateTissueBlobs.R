f7990a8349ec2e9ab80065117bbcde2529ecc2a9
sgunz <samuel.gunz@uzh.ch>
2024-01-26 13:51:02 +0100

    intial commit

---
+++ R/simulateTissueBlobs.R
@@ -0,0 +1,29 @@
+#' Simulate Tissue Blobs
+#'
+#' @param size the size of the binary image
+#' @param seedNumber number of seed points
+#' @param clumpSize the size of the Gaussian Kernel
+#'
+#' @return A binary image with simulated tissue blobs
+#' @export
+#'
+#' @examples
+#'
+simulateTissueBlobs <- function(size, seedNumber, clumpSize){
+  # Create an initial random image
+  image <- matrix(rep(0, size * size), nrow = size)
+
+  # Generate random indices
+  indices <- sample(size * size, seedNumber)
+
+  # Set these indices to 1
+  image[indices] <- 1
+
+  # Use a Gaussian filter to create the tissue clumps
+  image <- gblur(image, sigma = clumpSize)
+
+  # Threshold the final image to get a binary image
+  image <- image > mean(image)
+
+  return(image)
+}
