86d343e541fccc4d4890bf9af755e9a9834baad2
sgunz <samuel.gunz@uzh.ch>
2024-08-05 18:09:53 +0200

    new functions; checks failing

--- R/reconstructShapePPP.R
+++ R/reconstructShapePPP.R
@@ -105,6 +105,66 @@ reconstructShapeLRT <- function(ppp,
 
 
 
+#' Get intensity plot and intensity from spe object with given image id
+#'
+#' @param spe SpatialExperiment; a object of class `SpatialExperiment`
+#' @param marks character; name of column in `colData` that will correspond to the `ppp` marks
+#' @param image_col character; name of a column in `colData` that corresponds to the image
+#' @param image_id character; image id, must be present in image_col
+#' @param mark_select character; name of mark that is to be selected for the reconstruction
+#' @param bndw numeric; bandwith of the sigma parameter in the density estimation,
+#' if no value is given the bandwith is estimated using cross validation with the `bw.diggle` function.
+#' @param dim numeric; x dimension of the final reconstruction.
+#' A lower resolution speed up computation but lead to less exact reconstruction. Default = 500
+#' @return ggplot object with intensity image and histogram
+#' @export
+#'
+#' @examples
+shapeIntensityImage <- function(spe, marks,
+                              image_col,
+                              image_id,
+                              mark_select,
+                              bndw,
+                              dim = 500){
+
+  # Convert the spe object to a point pattern object
+  pp <- SPE2ppp(spe, marks = marks, image_col = image_col, image_id = image_id)
+
+  # Extract the islet cells
+  pp.islet <- subset(pp, marks %in% mark_select)
+
+  # Set the dimensions of the resulting reconstruction
+  dimyx <- getDimXY(pp.islet, dim)
+
+  # Set default of sigma bandwith
+  if (is.null(bndw)) bndw <- bw.diggle(pp.islet)
+
+  # plot the density of the image
+  im_df <- density(pp.islet, sigma = bndw, dimyx = dimyx) |> as.data.frame()
+
+  # plot density image
+  den_im <- im_df |>
+    ggplot(aes(x = x, y = y, color = value)) +
+    geom_tile() +
+    scale_color_viridis_c(option = "C") +
+    coord_equal() +
+    labs(color = "intensity") +
+    theme_classic()
+
+  # plot histogram
+  den_hist <- im_df |>
+    ggplot(aes(x = value)) +
+    geom_histogram(bins = 50) +
+    labs(x = "pixel intensity") +
+    theme_light()
+
+
+  p <- den_im + den_hist
+
+  return(p)
+}
+
+
 #' Reconstruct structure from spe object with given image id
 #'
 #' @param spe SpatialExperiment; a object of class `SpatialExperiment`
@@ -112,7 +172,8 @@ reconstructShapeLRT <- function(ppp,
 #' @param image_col character; name of a column in `colData` that corresponds to the image
 #' @param image_id character; image id, must be present in image_col
 #' @param mark_select character; name of mark that is to be selected for the reconstruction
-#' @param dim numeric; x dimension of the final reconstruction. Default = 500
+#' @param dim numeric; x dimension of the final reconstruction.
+#' A lower resolution speed up computation but lead to less exact reconstruction. Default = 500
 #' @param bndw numeric; bandwith of the sigma parameter in the density estimation,
 #' if no value is given the bandwith is estimated using cross validation with the `bw.diggle` function.
 #' @param thres numeric; intensity threshold for the reconstruction
@@ -133,7 +194,7 @@ reconstructShapeDensityImage <- function(spe, marks,
                 image_id)
 
   # Extract the islet cells
-  pp.islet <- subset(pp, marks == mark_select)
+  pp.islet <- subset(pp, marks %in% mark_select)
 
   # Get dimension of reconstruction
   dimyx <- getDimXY(pp.islet, dim)
@@ -157,11 +218,12 @@ reconstructShapeDensityImage <- function(spe, marks,
 #' @param marks character; name of column in `colData` that will correspond to the `ppp` marks
 #' @param image_col character; name of a column in `colData` that corresponds to the image
 #' @param mark_select character; name of mark that is to be selected for the reconstruction
-#' @param dim numeric; x dimension of the final reconstruction. Default = 500
+#' @param dim numeric; x dimension of the final reconstruction.
+#' A lower resolution speed up computation but lead to less exact reconstruction. Default = 500
 #' @param bndw numeric; bandwith of the sigma parameter in the density estimation,
 #' if no value is given the bandwith is estimated using cross validation with the `bw.diggle` function.
 #' @param thres numeric; intensity threshold for the reconstruction
-#' @param ncores numeric; nimer of cores for parallel processing using `mclapply`. Default = 1
+#' @param ncores numeric; number of cores for parallel processing using `mclapply`. Default = 1
 #'
 #' @importFrom parallel mclapply
 #' @importFrom dplyr bind_rows
@@ -191,3 +253,61 @@ reconstructShapeDensitySPE <- function(spe, marks,
   return(bind_rows(res_all))
 }
 
+#' Title
+#'
+#' @param spe SpatialExperiment; a object of class `SpatialExperiment`
+#' @param marks character; name of column in `colData` that will correspond to the `ppp` marks
+#' @param image_col character; name of a column in `colData` that corresponds to the image
+#' @param mark_select character; name of mark that is to be selected for the reconstruction
+#' @param nimages integer; number of images for the estimation. Will be randomly sampled
+#' @param fun character; function to estimate the kernel density. Default bw.diggle.
+#' @param ncores numeric; number of cores for parallel processing using `mclapply`. Default = 1
+#' @param plot_hist logical; if histogram of estimated densities shoul be plotted. Default = TRUE
+#'
+#' @importFrom parallel mclapply
+#' @import spatstat.explore
+#' @return list; list of estimated intensities
+#' @export
+#'
+#' @examples
+estimateKernelDensitySPE <- function(spe,
+                                     marks,
+                                     image_col,
+                                     mark_select = NULL,
+                                     nimages = NULL,
+                                     fun = "bw.diggle",
+                                     ncores = 5,
+                                     plot_hist = TRUE) {
+
+  all_images <- colData(spe)[[image_col]] |> unique()
+
+  if(is.null(nimages)) nimages <- length(all_images)
+
+  sample_images <- sample(all_images, nimages)
+
+  bndws <- parallel::mclapply(sample_images, function(x) {
+    pp <- SPE2ppp(spe,
+                  marks = marks,
+                  image_col = image_col,
+                  image_id = x)
+    # If selection of mark
+    if(!is.null(mark_select)) pp <- subset(pp, marks %in% mark_select)
+    # Estimate the bandwidth for the kernel estimation of point process intensity
+    bndw <- do.call(fun, args = list(X = pp, warn = FALSE))
+    return(bndw)
+  }, mc.cores = ncores)
+
+  if (plot_hist == TRUE & nimages > 1){
+
+    df <- as.data.frame(unlist(bndws))
+    colnames(df) <- "densities"
+
+    p <- df |>
+      ggplot(aes(x = densities)) +
+      geom_histogram(bins = nimages/2) +
+      theme_classic()
+    print(p)
+  }
+
+  return(unlist(bndws))
+}
